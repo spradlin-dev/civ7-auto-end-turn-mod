4941f7587c1e554f861b40cb6503c571eea58c7a
spradlin <jason@yeet.com>
2025-02-16 06:27:14 -0600

    Added sfx

--- toggle-auto-end-turn/ui/toggle-auto-end-turn.js
+++ toggle-auto-end-turn/ui/toggle-auto-end-turn.js
@@ -1,11 +1,12 @@
+import { Audio } from '/core/ui/audio-base/audio-support.js';
 
 function handleInput(event) {
     if (event.detail.name === 'md-toggle-auto-end-turn' && event.detail.status === InputActionStatuses.FINISH) {
         console.log(`AET: toggle-auto-end-turn: Toggling Auto End Turn from ${Configuration.getUser().isAutoEndTurn} to ${!Configuration.getUser().isAutoEndTurn}`);
         Configuration.getUser().setValue('AutoEndTurn', Configuration.getUser().isAutoEndTurn ? 0 : 1);
+        Audio.playSound("data-audio-choose-civ-activate", "new-civ-select");
         return false;
     }
-    return true;
 }
 
 window.addEventListener('engine-input', handleInput);
